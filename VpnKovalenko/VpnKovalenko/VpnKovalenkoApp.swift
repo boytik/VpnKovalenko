34955f5c989f85c9c859631b4203108afdd99320
Boytik Evgeny <slyfox1994@yandex.ru>
2025-07-24 16:08:02 +0300

    Add files via upload

---
+++ VpnKovalenko/VpnKovalenko/VpnKovalenkoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VpnKovalenkoApp.swift
+//  VpnKovalenko
+//
+//  Created by Евгений on 24.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct VpnKovalenkoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
